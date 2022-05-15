3e973d6d754c00a01a8501bad1d0fdd5586d0ec2
Pouya Tayebi <tayebi.pouya@gmail.com>
2022-05-15 11:53:02 +0430

    headers

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,15 +12,25 @@ const IndexPage = () => {
   const [starsCount, setStarsCount] = useState(0)
   useEffect(() => {
     // get data from GitHub api
-    fetch(`https://api.github.com/repos/gatsbyjs/gatsby`,{method:'GET',headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },})
-      .then(response => response.json()) // parse JSON from request
-      .then(resultData => {
-        setStarsCount(resultData.stargazers_count)
-        console.log('resultData',resultData)
-      }) // set data for the number of stars
+    
+   const getData=async (url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      //'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+    
+    const resultData=await getData(url="https://api.github.com/repos/gatsbyjs/gatsby")
+    setStarsCount(resultData.stargazers_count)
+    
   }, [])
 
   return (
